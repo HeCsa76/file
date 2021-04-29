577b8928ae83f3d95b3de4b1bbaf11831143c62b
Herczeg Csaba <hecsa76@gmail.com>
2021-04-29 11:34:45 +0200

    final

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,21 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook (Map<String, String> contacts, String output) throws IOException {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        FileWriter fileWriter = new FileWriter(output);
+
+        for (Map.Entry<String, String> entry : contacts.entrySet()) {
+            fileWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+        }
+
+        fileWriter.close();
+    }
+}
